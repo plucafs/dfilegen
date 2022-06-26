9be3258b8dbb6ce0e9e20246cec8e2ad863ec539
plucafs <l.poli@pm.me>
2022-06-26 15:28:16 +0200

    Save dialog remove
    One click save file (no confirmation dialog)
    Save file confirmation text
    Code formatting

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -5,29 +5,46 @@ onready var AppPathLineEdit = $CenterContainer/VBoxContainer/DesktopVariables/Ap
 onready var AppIcoLineEdit = $CenterContainer/VBoxContainer/DesktopVariables/AppIcoLineEdit
 onready var AppTypeLineEdit = $CenterContainer/VBoxContainer/DesktopVariables/AppTypeLineEdit
 onready var AppName = $CenterContainer/VBoxContainer/DesktopVariables/AppName
+onready var file_save = $CenterContainer/VBoxContainer/FileSaved
+onready var timer_file_save = $TimerFileSave
 
-onready var file_dialog = $CenterContainer/FileDialog
+func _ready():
+	file_save.hide()
 
-var directory = ""
-
-func _process(_delta):
-	if Input.is_action_just_pressed("ui_cancel"):
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 	
+#func save_file():
+#	var file = File.new()
+##	file.open("user://" + AppNameLineEdit.text + ".desktop", File.WRITE)
+#	file.open(str(directory) + AppNameLineEdit.text + ".desktop", File.WRITE)
+#	file.store_string("[Desktop Entry] \nName="+AppNameLineEdit.text+"\nExec="+AppPathLineEdit.text+"\nIcon="+AppIcoLineEdit.text+"\nEncoding=UTF-8\nVersion=1.0\nType="+AppTypeLineEdit.text+"\nTerminal=false")
+#	file.close()
+#	print("File saved!")
+##	file_path_dialog()
+
 func save_file():
 	var file = File.new()
-#	file.open("user://" + AppNameLineEdit.text + ".desktop", File.WRITE)
-	file.open(str(directory) + AppNameLineEdit.text + ".desktop", File.WRITE)
-	file.store_string("[Desktop Entry] \nName="+AppNameLineEdit.text+"\nExec="+AppPathLineEdit.text+"\nIcon="+AppIcoLineEdit.text+"\nEncoding=UTF-8\nVersion=1.0\nType="+AppTypeLineEdit.text+"\nTerminal=false")
+	file.open(AppNameLineEdit.text + ".desktop", File.WRITE)
+	file.store_string("[Desktop Entry]" + 
+	"\nName=" + AppNameLineEdit.text +
+	"\nExec=" + AppPathLineEdit.text +
+	"\nIcon=" + AppIcoLineEdit.text +
+	"\nEncoding=UTF-8" +
+	"\nVersion=1.0" +
+	"\nType=" + AppTypeLineEdit.text +
+	"\nTerminal=false")
 	file.close()
-	print("File saved!")
-#	file_path_dialog()
+	file_correctly_saved()
 	
-func _on_GenDeskFileButton_pressed():
-	file_dialog.show()
+func file_correctly_saved():
+	file_save.text = AppNameLineEdit.text + ".desktop is a success!"
+	timer_file_save.start()
+	file_save.show()
 
-func _on_FileDialog_dir_selected(dir):
-	dir = directory
+func _on_GenDeskFileButton_pressed():
 	save_file()
 
-	
+func _on_TimerFileSave_timeout():
+	file_save.hide()
