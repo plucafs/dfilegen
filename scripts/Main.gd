cb5771488087bac825866659acff1b21c3ea95ba
Luca Poli <l.poli@pm.me>
2023-07-03 20:03:02 +0200

    Add username detection
    Update .gitignore. Add icon and folder icons. Add fixed window dimensions and disabled resize. Escaped spaces in file path and icon. Update dialogs theme.

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,29 +1,43 @@
 extends Control
 
-# To do: create applications folder if it doesn't exist
+# TODO escape spaces
 
 onready var executable_path_button = $MarginContainer/Container/DesktopVariables/ExecutablePath.get_node("ButtonContainer/Button").connect("_on_Button_pressed", self, "_on_AppPathButton_pressed")
 onready var icon_path_button = $MarginContainer/Container/DesktopVariables/IconPath.get_node("ButtonContainer/Button").connect("_on_Button_pressed", self, "_on_AppIcoButton_pressed")
 onready var save_location_button = $MarginContainer/Container/DesktopVariables/SaveLocation.get_node("ButtonContainer/Button").connect("_on_Button_pressed", self, "_on_SaveLocationButton_pressed")
 
-onready var app_name = $MarginContainer/Container/DesktopVariables/Name.get_node("ButtonContainer/LineEdit")
-onready var executable_path = $MarginContainer/Container/DesktopVariables/ExecutablePath.get_node("ButtonContainer/LineEdit")
-onready var app_icon_path = $MarginContainer/Container/DesktopVariables/IconPath.get_node("ButtonContainer/LineEdit")
-onready var app_type = $MarginContainer/Container/DesktopVariables/Type.get_node("ButtonContainer/LineEdit")
-onready var save_location = $MarginContainer/Container/DesktopVariables/SaveLocation.get_node("ButtonContainer/LineEdit")
-onready var generate_file_button = $MarginContainer/Container/DesktopVariables/GenerateFileButton
-onready var file_saved_label = $MarginContainer/Container/FileSavedLabel
+onready var app_name = $"%Name".get_node("ButtonContainer/LineEdit")
+onready var executable_path = $"%ExecutablePath".get_node("ButtonContainer/LineEdit")
+onready var app_icon_path = $"%IconPath".get_node("ButtonContainer/LineEdit")
+onready var app_type = $"%Type".get_node("ButtonContainer/LineEdit")
+onready var save_location = $"%SaveLocation".get_node("ButtonContainer/LineEdit")
+onready var generate_file_button = $"%GenerateFileButton"
+onready var file_saved_label = $"%FileSavedLabel"
 
 onready var app_path_file_dialog = $FileDialog/AppPathFileDialog
 onready var app_ico_file_dialog = $FileDialog/AppIcoFileDialog
 onready var generate_file_dialog = $FileDialog/GenerateFileDialog
+onready var bg_dialog: ColorRect = $FileDialog/BgDialog
 
 onready var timer_file_save = $TimerFileSave
 
+var username: String = OS.get_data_dir().split("/")[2]
+var save_location_dir: String = "/home/%s/.local/share/applications/" % username
+var desktop_dir: String
+
 func _ready():
+	OS.window_resizable = false
+	OS.window_size = Vector2(500, 500)
 	app_name.grab_focus()
-	save_location.text = "/home/username/.local/share/applications/"
+	save_location.text = save_location_dir
 	file_saved_label.hide()
+	bg_dialog.hide()
+	desktop_dir = get_desktop_directory()
+
+func get_desktop_directory():
+	var output: Array
+	OS.execute("xdg-user-dir", ["DESKTOP"], true, output)
+	return output[0]
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -35,16 +49,17 @@ func save_file(path):
 	file.open(path + "/" + app_name.text + ".desktop", File.WRITE)
 	file.store_string("[Desktop Entry]" + 
 	"\nName=" + app_name.text +
-	"\nExec=" + executable_path.text +
-	"\nIcon=" + app_icon_path.text +
+	"\nExec=" + executable_path.text.replace(" ", "\\ ") +
+	"\nIcon=" + app_icon_path.text.replace(" ", "\\ ") +
 	"\nType=" + "Application" +
 	"\nEncoding=UTF-8" +
 	"\nVersion=1.0" +
 	"\nTerminal=false")
 	file.close()
-	file_correctly_saved()
+	create_link_to_desktop(app_name.text)
+	file_saved()
 	
-func file_correctly_saved():
+func file_saved():
 	file_saved_label.text = app_name.text + ".desktop is saved"
 	timer_file_save.start()
 	file_saved_label.show()
@@ -52,35 +67,45 @@ func file_correctly_saved():
 func _on_TimerFileSave_timeout():
 	file_saved_label.hide()
 
-### Select and apply executable path
-func _on_AppPathButton_pressed():
-#	app_path_file_dialog.rect_position = Vector2(0, OS.window_size.y/8)
-#	app_path_file_dialog.rect_size = Vector2(400,400)
-	app_path_file_dialog.show()
-
 func _on_AppPathFileDialog_file_selected(path):
 	executable_path.text = path
-###
-
-### Select and apply icon path
-func _on_AppIcoButton_pressed():
-#	app_ico_file_dialog.rect_position = Vector2(0, OS.window_size.y/8)
-#	app_ico_file_dialog.rect_size = Vector2(400,400)
-	app_ico_file_dialog.show()
-
+	
 func _on_AppIcoFileDialog_file_selected(path):
 	app_icon_path.text = path 
-###
-
-### Select and apply location path
-func _on_SaveLocationButton_pressed():
-#	app_path_file_dialog.rect_position = Vector2(0, OS.window_size.y/8)
-#	app_path_file_dialog.rect_size = Vector2(400,400)
-	app_path_file_dialog.show()
 
 func _on_GenerateFileDialog_dir_selected(dir: String) -> void:
 	save_location.text = dir
-###
+
+
+# on button pressed
+func _on_AppPathButton_pressed():
+	bg_dialog.show()
+	app_path_file_dialog.popup_centered()
+	
+func _on_AppIcoButton_pressed():
+	bg_dialog.show()
+	app_ico_file_dialog.popup_centered()
+
+func _on_SaveLocationButton_pressed():
+	bg_dialog.show()
+	app_path_file_dialog.popup_centered()
 
 func _on_GenerateFileButton_pressed() -> void:
 	save_file(save_location.text)
+#
+
+func create_link_to_desktop(name_of_app):
+	var dot_desktop_file_path = save_location_dir + "/" + name_of_app + ".desktop"
+	OS.execute("ln", [dot_desktop_file_path, desktop_dir])
+	pass
+
+# on dialog hide
+func _on_AppPathFileDialog_hide() -> void:
+	bg_dialog.hide()
+
+func _on_AppIcoFileDialog_hide() -> void:
+	bg_dialog.hide()
+
+func _on_GenerateFileDialog_hide() -> void:
+	bg_dialog.hide()
+#
--- scripts/SectionContainer.gd
+++ scripts/SectionContainer.gd
@@ -3,13 +3,15 @@ extends VBoxContainer
 export (String) var text_label
 export (String) var placeholder_line_edit
 export (bool) var is_button_visible
+export (Resource) var button_icon
 
 onready var label = $Label
 onready var line_edit = $ButtonContainer/LineEdit
 onready var button = $ButtonContainer/Button
 
 func _ready():
-	button.text = "Path"
+#	button.text = "Path"
+	button.icon = button_icon
 	if is_button_visible:
 		button.visible = true
 	else:
@@ -17,3 +19,4 @@ func _ready():
 		
 	label.text = text_label
 	line_edit.placeholder_text = placeholder_line_edit
+	
